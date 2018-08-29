5889c7f986dcd64c82b0ed3333a1d3586f0da197
Kragov <34142271+Kragov@users.noreply.github.com>
2018-08-29 11:56:24 +0300

    Add files via upload
    Finished test task

---
+++ js/slick.js
@@ -0,0 +1,9 @@
+    $(document).ready(function(){
+        $('.slider').slick({
+            autoplay: true,
+            autoplaySpeed: 7500,
+            dots: true,
+            loop: true,
+            arrows:false
+        });
+    });
